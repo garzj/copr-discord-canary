98a20c9032523bd005c9c6344f0f04f602560f0c
garzj <johannes@garz.dev>
2024-12-13 04:06:04 +0100

    feat: check new version script for automation

--- check_new_version.py
+++ check_new_version.py
@@ -1,53 +1,64 @@
 #!/usr/bin/python3
 
-""" Warning not complete """
+import re
+import subprocess
+import sys
 
 import requests
-import re
-import os
-import subprocess
-
-def runme(cmd, env, cwd='.'):
-    """Simple function to run a command and return 0 for success, 1 for
-       failure.  cmd is a list of the command and arguments, action is a
-       name for the action (for logging), pkg is the name of the package
-       being operated on, env is the environment dict, and cwd is where
-       the script should be executed from."""
-    try:
-        subprocess.check_call(cmd, env=env, cwd=cwd, stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as e:
-        sys.stderr.write('%s failed: %s\n' % (cmd, e))
-        return 1
-    return 0
 
 spec_file = "discord-canary.spec"
 
-h = requests.head("https://discord.com/api/download/canary?platform=linux&format=tar.gz")
-print(h.headers.get("location"))
-minor64 = h.headers.get("location")
-version64 = minor64.split("/")[5]
+do_update = "-u" in sys.argv
+
+h = requests.head(
+    "https://discord.com/api/download/canary?platform=linux&format=tar.gz"
+)
+dl_location = h.headers.get("location")
+if dl_location is None:
+    print(f"Request did not redirect to new location.")
+    exit(1)
+new_version = dl_location.split("/")[5]
 
 spec = open(spec_file).read()
-str_mx3 = re.compile(r'(Version:\s*) .*')
-spec3 = re.sub(str_mx3, r'\1 %s' % version64, spec)
+match = re.search(r"Version:\s+(.*)", spec)
+if match is None:
+    print(f"Version field not found in spec:", spec)
+    exit(1)
+cur_version = match.group(1)
 
-if spec != spec3:
-#    open(spec_file, 'w').write(spec3)
-    enviro = os.environ
-    pkgcmd = ['rpmdev-bumpspec', '-n', version64, '-c', 'Update to %s' % (version64),
-        spec_file]
-    if runme(pkgcmd, enviro):
-        print('error running runme')
+if cur_version == new_version:
+    print(f"Version: {cur_version}")
+    print("Already updated!")
+    exit(1 if do_update else 0)
 
-    print("New version available!")
-    print('spectool -g discord-canary.spec')
-    print('rfpkg mockbuild -N --default-mock-resultdir --root fedora-39-x86_64-rpmfusion_nonfree')
-else:
-    print("Already updated !")
-    print('spectool -g discord-canary.spec')
+print(f"Current version: {cur_version}")
+print(f"New version: {new_version}")
+print()
 
-print("rfpkg new-sources $(spectool -l --sources discord-canary.spec | grep https | sed 's/.*: //;s#.*/##')")
-print('rfpkg ci -c && git show && echo Press enter to push and build; read dummy; rfpkg push && rfpkg build --nowait')
-print('git checkout f41 && git merge master && git push && rfpkg build --nowait; git checkout master')
-print('git checkout f40 && git merge master && git push && rfpkg build --nowait; git checkout master')
-print('git checkout el9 && git merge master && git push && rfpkg build --nowait; git checkout master')
+if not do_update:
+    print("Pass -u to update.")
+    exit(0)
+
+changelog = f"Update to {new_version}"
+cmd = [
+    "tito",
+    "tag",
+    "--use-version",
+    new_version,
+    "--changelog",
+    changelog,
+    "--accept-auto-changelog",
+]
+print("+", " ".join(cmd))
+
+try:
+    subprocess.check_call(cmd, stderr=subprocess.PIPE)
+except FileNotFoundError:
+    print("Command not found. Is tito installed?")
+    exit(1)
+except subprocess.CalledProcessError as e:
+    print("Command failed:", e)
+    exit(1)
+
+print()
+print("Updated to version:", new_version)
